02cc7407b887e74a471a15e0dcb0cb24ddeb936c
kunfayz6-lab <kunfayz6@gmail.com>
2025-10-19 18:46:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def main():
 
     while True:
         print("\033[32m[]└> ".format(len(socket_list)))
-        print("\033[95m[]└> \033[33mSaddam-DDos \033[3m••> \033[38;5;206m" +str(url)+ "   |\033[0m")
+        print("\033[95m[]└> \033[33mSaddam-DDos \033[3m••> \033[38;5;206m" +str(ip)+ "   |\033[0m")
 
         for s in socket_list:
             try:
@@ -135,7 +135,7 @@ def main():
 
         for _ in range(socket_count - len(socket_list)):
             print("\033[32m[]└> ".format(len(socket_list)))
-            print("\033[95m[]└> \033[33mSaddam-DDos \033[3m••> \033[38;5;206m" +str(url)+ "   |\033[0m")
+            print("\033[95m[]└> \033[33mSaddam-DDos \033[3m••> \033[38;5;206m" +str(ip)+ "   |\033[0m")
             try:
                 s=init_socket(ip,port)
                 if s:
