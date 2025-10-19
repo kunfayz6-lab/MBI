8b5e3febbd71e330ae175ce85ba5ae3587df75e7
kunfayz6-lab <kunfayz6@gmail.com>
2025-10-19 21:27:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -124,9 +124,9 @@ def main():
     bar.finish()
 
     while True:
-        print("\033[32m[]•> ".format(len(socket_list)))
-        print("\033[38;5;22m[]•>  \033[38;5;39m[ S A D D A M ] \033[31m•>\033[33m Running attack \033[38;5;206m" +str(ip)+ " |🇵🇸 \033[0m")
-        print("\033[38;5;166m[]•>  \033[96m[ S A D D A M ] \033[34m•>\033[37m Running attack \033[38;5;111m" +str(ip)+ " |🇵🇸\033[0m")
+        print("\033[32m—— ".format(len(socket_list)))
+        print("\033[38;5;22m└>  \033[38;5;39m[ S A D D A M ] \033[31m•>\033[33m Running attack \033[38;5;206m" +str(ip)+ " |🇵🇸 \033[0m")
+        print("\033[38;5;166m└>  \033[96m[ S A D D A M ] \033[34m•>\033[37m Running attack \033[38;5;111m" +str(ip)+ " |🇵🇸\033[0m")
 
         for s in socket_list:
             try:
@@ -135,9 +135,9 @@ def main():
                 socket_list.remove(s)
 
         for _ in range(socket_count - len(socket_list)):
-            print("\033[32m[]•> ".format(len(socket_list)))
-            print("\033[38;5;22m[]•>  \033[33m[ S A D D A M ] \033[31m•>\033[33m Running attack \033[38;5;206m" +str(ip)+ " |🇵🇸\033[0m")
-            print("\033[38;5;166m[]•>  \033[96m[ S A D D A M ] \033[34m•>\033[37m Running attack \033[38;5;111m" +str(ip)+ " |🇵🇸\033[0m")
+            print("\033[32m—— ".format(len(socket_list)))
+            print("\033[38;5;22m└>  \033[33m[ S A D D A M ] \033[31m•>\033[33m Running attack \033[38;5;206m" +str(ip)+ " |🇵🇸\033[0m")
+            print("\033[38;5;166m└>  \033[96m[ S A D D A M ] \033[34m•>\033[37m Running attack \033[38;5;111m" +str(ip)+ " |🇵🇸\033[0m")
             try:
                 s=init_socket(ip,port)
                 if s:
